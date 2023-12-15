15fec525a2b01df474f4a55f09bef3508ef0b4d9
Pedro Henrique <pedrohrs39@gmail.com>
2023-12-14 22:37:06 -0300

    vary bad solution

--- src/aplication/Progan.java
+++ src/aplication/Progan.java
@@ -18,8 +18,11 @@ public class Progan {
         Date checkin = sdf.parse(scan.next());
         System.out.print("Chack-out date (dd/MM/yyyy): " );
         Date checkout = sdf.parse(scan.next());
-
-        if (!checkout.after(checkin)){
+        Date now = new Date();
+        if(checkin.before(now) || checkout.before(now)){
+            System.out.println("error in reservation: Reservation dates fur update must be future dates. ");
+        }
+        else if (!checkout.after(checkin)){
             System.out.print("error in reservation: check out dat must be after chack-in date");
         }else{
             Reservation reservation = new Reservation(number, checkin, checkout);
@@ -32,7 +35,7 @@ public class Progan {
             System.out.print("Chack-out date (dd/MM/yyyy): " );
             checkout = sdf.parse(scan.next());
 
-            Date now = new Date();
+            now = new Date();
             if (checkin.before(now) || checkout.before(now)){
                 System.out.println("error in reservation: Reservation dates fur update must be future dates. ");
             }else if(!checkout.after(checkin)){
@@ -46,5 +49,5 @@ public class Progan {
     }
 }
 
-// aula 174 tempo 7:43
+// aula 175 
 // verificar como foi criado a class Reservation
